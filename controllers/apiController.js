7f2b11832639f6438fc6c56bfaf350d8e1c3eef5
Andri Purnomo <56879512+andripurnomo87@users.noreply.github.com>
2021-08-11 23:15:39 +0700

    Revert "resolve contrroller data room & add http status code"
    This reverts commit 2541aaf864f024226163de2dd5c828af1249505b.

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -16,22 +16,22 @@ module.exports = {
   signUp: async (req, res) => {
     try {
       await User.register(req.body);
-      res.status(201).json({ message: "Register berhasil, silahkan login" });
+      res.json({ message: "Register berhasil, silahkan login" });
     } catch (error) {
-      res.status(400).json({ message: "Periksa kembali data data register anda" });
+      res.json({ message: "Periksa kembali data data register anda" });
     }
   },
   signIn: async (req, res) => {
     try {
       const user = await User.authenticate(req.body);
-      res.status(200).json(format(user));
+      res.json(format(user));
     } catch (error) {
-      res.status(400).json({ message: "Periksa kembali data data login anda" });
+      res.json({ message: "Periksa kembali data data login anda" });
     }
   },
   whoAmi: (req, res) => {
     const currentUser = req.user;
-    res.status(200).json(currentUser);
+    res.json(currentUser);
   },
   createRoom: async (req, res) => {
     try {
@@ -40,21 +40,22 @@ module.exports = {
         id: uuidv4(),
         playerOneId: user.id,
       });
-      res.status(201).json({
+      res.json({
         message: `Berhasil generate room dengan id : ${room.id}`,
         room,
       });
     } catch (error) {
-      res.status(400).json({ message: "userId tidak ditemukan" });
+      res.json({ message: "userId tidak ditemukan" });
     }
   },
-  viewDataRoom: async (req, res) => {
-    try {
-      const room = await Room.findOne({ where: { id: req.params.id } });
-      res.status(200).json(room);
-    } catch (error) {
-      res.status(400).json({ message: "Id room tidak ditemukan" });
-    }
+  viewDataRoom: (req, res) => {
+    User.findAll({ include: Room })
+      .then((p1) => {
+        res.json(p1);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
   },
   joinRoom: async (req, res) => {
     try {
@@ -68,9 +69,9 @@ module.exports = {
           },
         }
       );
-      res.status(200).json({ message: "berhasil join room" });
+      res.json({ message: "berhasil join room" });
     } catch (error) {
-      res.status(400).json({ message: "Gagal join room" });
+      res.json({ message: "Gagal join room" });
     }
   },
 };
