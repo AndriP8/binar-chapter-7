10484d37ffc10f70b8d634c832f0bb9781ac7006
Andri Purnomo <56879512+andripurnomo87@users.noreply.github.com>
2021-08-11 23:30:47 +0700

    add validation token in endpoint login

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
 const apiController = require("../controllers/apiController");
 const restrict = require("../middlewares/restrict-jwt");
+const token = require("../middlewares/check-token");
 
 router.post("/v1/register", apiController.signUp);
-router.post("/v1/login", apiController.signIn);
+router.post("/v1/login", token, apiController.signIn);
 router.get("/v1/whoami", restrict, apiController.whoAmi);
 
 router.post("/generate/:id", apiController.createRoom);
-router.get("/generate/:playerOneId", apiController.viewDataRoom);
+router.get("/generate/:id", apiController.viewDataRoom);
 router.post("/room_id", apiController.joinRoom);
 
 module.exports = router;
