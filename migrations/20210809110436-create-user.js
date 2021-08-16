41b843a2e4bcbbdf1df28e48b3351e207fac5f1d
Andri Purnomo <56879512+andripurnomo87@users.noreply.github.com>
2021-08-16 11:06:04 +0700

    change datatype id migration user

--- migrations/20210809110436-create-user.js
+++ migrations/20210809110436-create-user.js
@@ -7,8 +7,9 @@ module.exports = {
     await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
+        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.UUID,
+        type: Sequelize.INTEGER,
       },
       username: {
         type: Sequelize.STRING,
